5dc9be4467e38de8608aca4bd7b4bec10d675b1b
iuli <iuliobreja23@gmail.com>
2023-10-17 19:49:21 +0300

    FE: Logic-exercises-> ex 2 - bug fixed?

--- ex-2-convert-arabic-to-roman.js
+++ ex-2-convert-arabic-to-roman.js
@@ -9,18 +9,18 @@ function convertToRoman(num) {
 
   const romanNumeralsDictionary = {
     'M': 1000,
-    'CM':	900,
+    'CM': 900,
     'D': 500,
     'CD': 400,
     'C': 100,
-    'XC':	90,
+    'XC': 90,
     'L': 50,
-    'XL':	40,
+    'XL': 40,
     'X': 10,
-    'IX':	9,
-    'V':	5,
-    'IV':	4,
-    'I':	1
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
   };
 
   for (let romanNumberKey in romanNumeralsDictionary) {
